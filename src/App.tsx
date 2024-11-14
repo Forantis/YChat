35235178a5e9c1d7883b1e33404c2a99511e4f6d
Côme Sarazin <tech.dev@team-one.fr>
2024-11-14 14:20:34 +0100

    Ajout de la page d'accueil avec le composant LandingPage et mise à jour des styles CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 //import { useState } from 'react'
 import NavBar from './components/UI/NavBar/NavBar'
+import LandingPage from './components/LandingPage/LandingPage'
 import Footer from './components/UI/Footer/Footer'
 import './App.css'
+
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="container">
-        <h1>Page d'accueil</h1>
-        <p>Bienvenue sur notre site !</p>
-      </div>
+      <LandingPage />
       <Footer />
     </>
   )
---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './LandingPage.scss';
+import Button from '../UI/Button/Button';
+
+const LandingPage: React.FC = () => {
+    return (
+
+        <div className="landing-page">
+            <div className='landing-page__background'>
+                <img src="./images/Image_LP.png" alt="YChat logo" className="images_LP">
+                </img> 
+            </div>  
+            <div className="landing-page">
+                <h1>Bienvenue sur YChat</h1>
+                <p>Connectez-vous avec vos amis et votre famille.</p>
+                <Button onClick={() => console.log('Inscription button clicked')}>Inscription</Button>
+                <Button onClick={() => console.log('Connexion button clicked')}>Connexion</Button>
+                <section className="landing-page__features">
+                    <h2>Fonctionnalités</h2>
+                    <ul>
+                        <li>Messagerie instantanée</li>
+                        <li>Groupes de discussion</li>
+                        <li>Partage de photos et de messages vocaux</li>
+                    </ul>
+                </section>
+            </div>
+        </div>    
+    );
+};
+
+export default LandingPage;
