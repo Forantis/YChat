ec57f3ac379a0797af5e4df565c9403a894e5260
Forantis <antoine.forest.08@hotmail.fr>
2024-11-14 12:00:33 +0100

    Refactor Convex API to include messages and users modules

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { api } from "../convex/_generated/api";
 
 function App() {
   const messages = useQuery(api.messages.get);
+  console.log(messages);
   return (
     <div className="App">
       {messages?.map(({ _id, body }) => <div key={_id}>{body}</div>)}
