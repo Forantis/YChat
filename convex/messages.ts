e96ea200f2b19fdac6506a64dc7f98c0d8c92794
Forantis <antoine.forest.08@hotmail.fr>
2024-11-20 23:10:49 +0100

    add query to retrieve messages by conversation ID and order by creation date

--- convex/messages.ts
+++ convex/messages.ts
@@ -9,11 +9,23 @@ export const get = query({
   },
 });
 
+
 export const getBySenderId =query({
   args: { sender_id: v.number() },
   handler: async (ctx, args) => {
     return await ctx.db.query("messages")
     .filter((q) => q.eq(q.field("sender_id"), args.sender_id))
+    .collect();
+  },
+});
+
+// Get all messages by conversation id order by created_at date in descending order
+export const getByConversationId = query({
+  args: { conversation_id: v.number() },
+  handler: async (ctx, args) => {
+    return await ctx.db.query("messages")
+    .filter((q) => q.eq(q.field("conversation_public_uuid"), args.conversation_id))
+    .order("desc")
     .collect();
   },
 });
