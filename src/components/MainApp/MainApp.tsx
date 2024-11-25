385cd511e8d75a9d333369da9d3bcf1d2eb7ab01
Forantis <antoine.forest.08@hotmail.fr>
2024-11-25 21:49:07 +0100

    refactor MainApp component to integrate ConversationsList and MessagesList, updating state management for selected conversation

--- src/components/MainApp/MainApp.tsx
+++ src/components/MainApp/MainApp.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { api } from "../../../convex/_generated/api";
 import { useQuery, useMutation } from "convex/react";
+import ConversationsList from "../UI/ConversationsList/ConversationsList";
+import MessagesList from "../UI/MessagesList/MessagesList";
 
 export default function MainApp(){
   const [sender, setSender] = useState(0);
@@ -12,10 +14,11 @@ export default function MainApp(){
   const [tokenIdentifier, setTokenIdentifier] = useState("");
   const [public_uuid, setPublic_uuid] = useState(0);
   const [role, setRole] = useState("");
-  const [user_id, setUser_id] = useState(0);
+  const [user_id, setUser_id] = useState(2);
   const [conversation_id, setConversation_id] = useState(0);
+  const [selectedConversation, setSelectedConversation] = useState(0);
 
-  const messagesByConversationId = useQuery(api.messages.getByConversationId, { conversation_id: conversation_id });
+  const messagesByConversationId = useQuery(api.messages.getByConversationId, { conversation_id: selectedConversation });
   const registerTest = useMutation(api.users.register);
   const conversationsByUserId = useQuery(api.conversations.getConversationsNameByUserId, { user_id });
 
@@ -29,69 +32,11 @@ export default function MainApp(){
 
   return (
     <div className="App">
-            <input
-              type="number"
-              value={sender}
-              onChange={(e) => setSender(parseInt(e.target.value))}
-            />
-            <input
-              type="text"
-              placeholder="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input 
-            type="text" 
-            placeholder="name" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            />
-            <input
-            type="text"
-            placeholder="surname"
-            value={surname}
-            onChange={(e) => setSurname(e.target.value)}
-            />
-
-            <button
-              onClick={async () => {
-                handleRegister();
-                await registerTest({ 
-                  email, 
-                  password, 
-                  name, 
-                  surname, 
-                  created_at, 
-                  tokenIdentifier, 
-                  public_uuid, 
-                  role });
-              }}
-            > Login Test</button>
-
-            <section id="getConversationsByUserIdTest">
-              <h2>get Conversations By User Id Test</h2>
-              <input type="number" value={user_id} onChange={(e) => setUser_id(parseInt(e.target.value))} />
-              <div>
-                {conversationsByUserId?.map(({ _id, conversation_name, conversation_public_uuid }) => <button key={_id} value={conversation_public_uuid} onClick={
-                  async (e) => {
-                    setConversation_id(parseInt(e.currentTarget.value));
-                  }
-                }>{conversation_name}</button>)}
-              </div>
-            </section>
-
-            <section id="getMessagesByConversationIdTest">
-              <h2>get Messages By Conversation Id Test</h2>
-              <div>
-                {messagesByConversationId?.map(({ _id, body, sender_id }) => <div key={_id} className={sender_id === user_id? "right" : "left"}>{body}</div>)}
-              </div>
-            </section>
-          </div>
+     <ConversationsList 
+     conversations={conversationsByUserId} 
+     selectedConversation={selectedConversation} 
+     setSelectedConversation={setSelectedConversation} />
+      <MessagesList messages={messagesByConversationId} user_id={user_id} />
+    </div>
   );
 }
