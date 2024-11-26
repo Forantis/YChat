957eea30092509adb6be800477a7fe602768015a
Forantis <antoine.forest.08@hotmail.fr>
2024-11-26 23:05:22 +0100

    style: update ConversationCard component with new class names and import styles

--- src/components/UI/ConversationCard/ConversationCard.tsx
+++ src/components/UI/ConversationCard/ConversationCard.tsx
@@ -1,3 +1,5 @@
+import './styles.scss'
+
 interface Conversation {
   conversation_name: string;
   conversation_public_uuid: string;
@@ -6,8 +8,8 @@ interface Conversation {
 export default function ConversationCard({ conversation, setSelectedConversation }: { conversation: Conversation, setSelectedConversation: (conversation: Conversation) => void }) {
   return (
     <div className="conversation-card" onClick={() => setSelectedConversation(conversation.conversation_public_uuid)}>
-      <h3>{conversation.conversation_name}</h3>
-      <p>{conversation.conversation_public_uuid}</p>
+      <h3 className="conversation-card__title">{conversation.conversation_name}</h3>
+      <p className="conversation-card__last-message">{conversation.conversation_public_uuid}</p>
     </div>
   );
 }
