4429d49cd7519f196f1d9a89406159ff4135fd79
Forantis <antoine.forest.08@hotmail.fr>
2024-11-27 21:44:08 +0100

    feat: enhance ConversationCard to display last message read status and update styles for better layout

--- src/components/UI/ConversationCard/ConversationCard.tsx
+++ src/components/UI/ConversationCard/ConversationCard.tsx
@@ -8,14 +8,17 @@ interface Conversation {
 }
 
 export default function ConversationCard({ conversation, setSelectedConversation }: { conversation: Conversation, setSelectedConversation: (conversation: Conversation) => void }) {
-  const { conversation_name, conversation_public_uuid, last_update } = conversation;
+  const { conversation_name, conversation_public_uuid} = conversation;
   const lastMessage = useQuery(api.messages.getLastMessageByConversationId, { conversation_id: conversation_public_uuid });
   
   // Show the time of the last message in the conversation card and the last message
   let shownDateOrTime;
   let lastMessageText;
+  let readStatus;
+
   if(lastMessage){
   lastMessageText = lastMessage[0].body;
+  readStatus = lastMessage[0].read_status;
   const now = new Date();
   const lastUpdateDate = new Date(lastMessage[0].created_at);
   const timeDifference = now.getTime() - lastUpdateDate.getTime();
@@ -35,8 +38,11 @@ export default function ConversationCard({ conversation, setSelectedConversation
         <h3 className="conversation-card__content__title">{conversation_name}</h3>
         <p className="conversation-card__content__last-message">{lastMessageText}</p>
       </div>
-      
-      <p className="conversation-card__last-update">{shownDateOrTime}</p>
+      <div className='conversation-card__infos'>
+      <p className="conversation-card__infos__last-update">{shownDateOrTime}</p>
+      {readStatus === 'read' ? <p className="conversation-card__infos__read-status">Read</p> 
+      : <p className="conversation-card__infos__read-status"> Unread</p>}
+      </div>
     </div>
   );
 }
